9a8e300ca1e9254183a83c44a5a287cce5a07404
HugoLamb <hugolamb2003@gmail.com>
2025-04-01 09:40:14 +0200

    footer/about
    Footer et about a sans style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { SummonerDetailsComponent } from './summoner-details/summoner-details.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    {path: 'summoner/:region/:summonerName', component:SummonerDetailsComponent}
+    {path: 'summoner/:region/:summonerName', component:SummonerDetailsComponent},
+    { path: 'about', component: AboutComponent },
 ];

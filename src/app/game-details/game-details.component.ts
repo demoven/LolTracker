df4c75eac3c9610307cf5bd57c10551dda2cd4cb
mrreyleo <98902675+Mrreyleo@users.noreply.github.com>
2025-04-01 15:49:09 +0200

    player details in game details

--- src/app/game-details/game-details.component.ts
+++ src/app/game-details/game-details.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Game } from '../interfaces/game';
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-game-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './game-details.component.html',
   styleUrl: './game-details.component.css'
 })
 export class GameDetailsComponent implements OnInit {
   dataService = inject(DataService)
+  region!:string
   game:Game | null =null
   gameVersion!:string
   gameId!:string
@@ -22,7 +23,13 @@ export class GameDetailsComponent implements OnInit {
       this.gameVersion=params.get('gameVersion') ?? ''
     })
     this.dataService.getDetailedMatchById(this.gameId).subscribe(data =>this.game=data)
+    this.route.params.subscribe(params => {
+      const fullValue = params['gameId']; // "EUW1_7353440768"
+      this.region = fullValue.split('_')[0]; // Takes "EUW1"
+    });
   }
+
+
   
 
 
